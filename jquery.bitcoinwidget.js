47b2e00a73e350c2e2eaf8bd346c5c3597563028
Lukasz Gradzki <gradzki@o2.pl>
2012-05-13 17:40:57 +0200

    Basic widget shape

--- jquery.bitcoinwidget.js
+++ jquery.bitcoinwidget.js
@@ -3,9 +3,18 @@
 	var default_settings = {
 		css: {
 			button: {
-				'cursor': 'pointer'
-			}
-		}
+			    'max-width': 300 
+			}, // Additional CSS for the button
+			counter: {
+			    'max-width': 100
+			} // Additional CSS for the counter element
+		},
+		text: 'Bitcoin', // Button text
+		_button_element: null, // Button element
+		_counter_element: null, // Counter element
+		button: true, // Display button ?
+		counter: true, // Display counter ?
+		amount: '0.00' // Amount that is displayed by the counter
  	};
 	var methods = {
  		init : function( options ) {
@@ -17,6 +26,21 @@
  					settings = data;
        			if(options)
     				$.extend(true, settings, options);
+    			$this.empty().addClass('jq-bitcoin-widget');
+    			if (settings.button){
+    			    settings._button_element = $('<div></div>').addClass('button element');
+    			    settings._button_element.css(settings.css.button);
+    			    settings._button_element.text(settings.text);
+    			    $this.append(settings._button_element);
+    			}
+    			if (settings.counter){
+    			    var val = $('<span></span>');
+                    settings._counter_element = $('<div></div>').addClass('counter element');
+                    settings._counter_element.css(settings.css.counter);
+                    val.append(settings.amount);
+                    settings._counter_element.append(val);
+                    $this.append(settings._counter_element);
+                }
     			$this.data('BitcoinButton', settings);
        		});
        		return this;
